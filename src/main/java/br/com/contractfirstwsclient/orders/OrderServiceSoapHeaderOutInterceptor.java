9cbd1106be53f05b5ed4de82d9d84e309ce3d66b
demetriushenry <demetriushenry@gmail.com>
2016-06-10 16:50:47 -0300

    atualizando client

--- src/main/java/br/com/contractfirstwsclient/orders/OrderServiceSoapHeaderOutInterceptor.java
+++ src/main/java/br/com/contractfirstwsclient/orders/OrderServiceSoapHeaderOutInterceptor.java
@@ -1,5 +1,6 @@
 package br.com.contractfirstwsclient.orders;
 
+import javax.xml.bind.JAXBException;
 import javax.xml.namespace.QName;
 
 import org.apache.cxf.binding.soap.SoapHeader;
@@ -10,22 +11,35 @@ import org.apache.cxf.interceptor.Fault;
 import org.apache.cxf.jaxb.JAXBDataBinding;
 import org.apache.cxf.phase.Phase;
 
+/**
+ * Classe OutInterceptor customizada CXF
+ * 
+ * @author demetrius.v.oliveira
+ *
+ */
 public class OrderServiceSoapHeaderOutInterceptor extends AbstractSoapInterceptor {
-
+	
+	
+	/**
+	 * Construtor setando tipo de Phase
+	 */
 	public OrderServiceSoapHeaderOutInterceptor() {
 		super(Phase.WRITE);
 		this.addBefore(SoapOutInterceptor.class.getName());
 	}
 
+	/** 
+	 * Metodo da interface
+	 */
 	@Override
-	public void handleMessage(SoapMessage message) throws Fault {
-		QName qName = new QName("http://www.site.com/service/Orders/", "apikey");
-		String apikey = "a1b2c3d4e5";
+	public void handleMessage(final SoapMessage message) throws Fault {
+		final QName qName = new QName("http://www.site.com/service/Orders/", "apikey");
+		final String apikey = "a1b2c3d4e5";
 		
 		try {
-			SoapHeader header = new SoapHeader(qName, apikey, new JAXBDataBinding(apikey.getClass()));
+			final SoapHeader header = new SoapHeader(qName, apikey, new JAXBDataBinding(apikey.getClass()));
 			message.getHeaders().add(header);
-		} catch (Exception e) {
+		} catch (JAXBException e) {
 			throw new Fault(e);
 		}
 	}
--- src/main/java/br/com/contractfirstwsclient/orders/OrdersController.java
+++ src/main/java/br/com/contractfirstwsclient/orders/OrdersController.java
@@ -9,26 +9,56 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.site.schema.order.ObjectFactory;
 import com.site.schema.order.OrderInquiryResponseType;
 import com.site.schema.order.OrderInquiryType;
+import com.site.schema.order.OrderType;
 import com.site.service.orders.Orders;
 
+/**
+ * Classe do controlador da pagina processOrderPlacement
+ * 
+ * @author demetrius.v.oliveira
+ */
 @Controller("/processOrderPlacement")
 public class OrdersController {
 	
+	/**
+	 * Interface Webservice
+	 */
 	@Autowired
 	private Orders orders;
+	private ObjectFactory factory;
 	
+
+	/**
+	 * Construtor
+	 */
+	public OrdersController() {
+		factory = new ObjectFactory();
+	}
+
+	/**
+	 * Metodo controlador processOrderPlacement
+	 * que chama as classes request e response
+	 * @author demetrius.v.oliveira
+	 * @param model - recebe valores a ser mostrados na tela
+	 * @return - processa a pagina
+	 */
 	@RequestMapping(method = RequestMethod.GET)
-	public String processOrderPlacement(ModelMap model) throws Exception {
-		OrderInquiryType orderInquiry = new ObjectFactory().createOrderInquiryType();
+	public String processOrderPlacement(ModelMap model) {
+		OrderInquiryType orderInquiry = factory.createOrderInquiryType();
 		orderInquiry.setAccountId(1);
 		orderInquiry.setEan13(2235456567L);
 		orderInquiry.setOrderQuantity(2);
 		orderInquiry.setUniqueOrderId(999);
-		
+
 		OrderInquiryResponseType response = orders.processOrderPlacement(orderInquiry);
-		model.addAttribute("orderStatus", response.getOrder().getOrderStatus());
-		
+		model.addAttribute("orderStatus", getStatus(response));
+
 		return "processOrderPlacement";
+	}
+	
+	private String getStatus(OrderInquiryResponseType response) {
+		OrderType orderType = response.getOrder();
+		return orderType.getOrderStatus();
 	}
 
 }
